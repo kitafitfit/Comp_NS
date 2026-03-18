1ef1a94127b47a5a9f0f3df2a634757e5f094ed3
kitafitfit <82151008+kitafitfit@users.noreply.github.com>
2026-03-18 13:29:52 +0000

    functions defined

--- bayesian.py
+++ bayesian.py
@@ -1,1 +1,18 @@
+def validate_input(label):
+    while True: 
+        try:
+            user_input = float(input(f"{label} = "))
+            break
+        except ValueError:
+            print("Invalid entry, please type a number")
+    return user_input
 
+def bayesian(prior, likelihood, evidence):
+    posterior = prior * likelihood / evidence
+    return posterior
+
+prior = validate_input("prior")
+likelihood = validate_input("likelihood")
+evidence = validate_input("evidence")
+
+print("Result is =", bayesian(prior, likelihood, evidence))
